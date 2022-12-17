c4f421bcb47ad88fbf8c8ba1eb8e36b453d95a34
Shannon Reidy <shannonf.reidy@gmail.com>
2022-12-16 19:09:03 -0600

    image list now displays full images, no clipping and small gaps

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,20 +1,27 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const ImageCard = ({image}) => {
     const {description, urls} = image
+    const [spans, setSpans] = useState(0);
 
     const imageRef = useRef(null);
 
     useEffect(()=>{
-        imageRef.current.addEventListener('load', setSpans);
+        imageRef.current.addEventListener('load', getSpans);
     })
 
-    const setSpans = () => {
-        
+    const getSpans = () => {
+       const height= imageRef.current.clientHeight;
+
+       const ourSpans = Math.ceil(height/10);
+
+       setSpans(ourSpans);
+
+       
     }
 
     return(
-        <div>
+        <div style={{gridRowEnd:`span ${spans}`}}>
              <img 
              ref={imageRef}
              alt={description}
